29a7684366749fd611c573312fb3ffcec3202994
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2024-04-28 15:33:32 -0300

    trocando e melhorando endpoits

--- src/main/java/com/drummond/IA/web/controller/UsuarioController.java
+++ src/main/java/com/drummond/IA/web/controller/UsuarioController.java
@@ -17,30 +17,30 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
-    @PostMapping("/cadastro")
+    @PostMapping("/cadastrar")
     public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto) {
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toResponseDto(user));
     }
-    @GetMapping("/{id}")
+    @GetMapping("/buscar/{id}")
     @PreAuthorize("hasRole('ADMIN') or (hasRole('ALUNO') AND #id == authentication.principal.id)")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toResponseDto(user));
     }
 
-    @PatchMapping("/{id}")
+    @PatchMapping("/alterar/{id}")
     @PreAuthorize("hasAnyRole('ADMIN', 'ALUNO')  AND (#id == authentication.principal.id)")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDto dto) {
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
         return ResponseEntity.noContent().build();
     }
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/deletar/{id}")
     @PreAuthorize("hasRole('ADMIN') or (hasRole('ALUNO') AND #id == authentication.principal.id)")
     public ResponseEntity<Void> deleteUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioDelete dto){
         usuarioService.deletarUsuario(id,dto.getPassword(),dto.getConfirmaPasword());
@@ -54,9 +54,5 @@ public class UsuarioController {
         List<Usuario> users = usuarioService.buscarTodos();
         return ResponseEntity.ok(UsuarioMapper.toListaDto(users));
     }
-    // funcões especificas do admin
-
-
-
 
 }
