c02e58983daa894beb6e384210252e4f1235014f
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2024-05-19 15:37:43 -0300

    adicionando refresh token

--- src/main/java/com/drummond/IA/jwt/JwtUtils.java
+++ src/main/java/com/drummond/IA/jwt/JwtUtils.java
@@ -20,21 +20,24 @@ public class JwtUtils {
     public static final String SECRET_KEY = "0123456789-0123456789-0123456789";
     public static final long EXPIRE_DAYS = 0;
     public static final long EXPIRE_HOURS = 0;
-    public static final long EXPIRE_MINUTES = 30;
+    public static final long EXPIRE_MINUTES = 2;
+
+    //refresh
+    private static final long REFRESH_EXPIRE_DAYS =3;
     private JwtUtils(){
     }
 
     private static Key generateKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
     }
-    private static Date toExpireDate(Date start) {
+    private static Date toExpireDateAcess(Date start) {
         LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
         LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES);
         return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
     }
     public static JwtToken createToken(String id,String email, String role) {
         Date issuedAt = new Date();
-        Date limit = toExpireDate(issuedAt);
+        Date limit = toExpireDateAcess(issuedAt);
 
         String token = Jwts.builder()
                 .setHeaderParam("typ", "JWT")
@@ -48,6 +51,29 @@ public class JwtUtils {
 
         return new JwtToken(token);
     }
+
+    //criar refresh token
+
+    public static RefreshToken createRefreshToken(String id, String email){
+        Date issuedAt = new Date();
+        Date limit = toExpireDate(issuedAt,0,0,3);
+        String token = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setSubject(email)
+                .claim("id", id)
+                .setIssuedAt(issuedAt)
+                .setExpiration(limit)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .compact();
+        return  new RefreshToken(token);
+
+    }
+    private static Date toExpireDate(Date start, long days, long hours, long minutes) {
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime end = dateTime.plusDays(days).plusHours(hours).plusMinutes(minutes);
+        return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
     private static Claims getClaimsFromToken(String token) {
         try {
             return Jwts.parserBuilder()
